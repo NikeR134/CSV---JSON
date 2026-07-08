2ae0310ec58df8027a8ad216478327cc6c4196a1
NikeR134 <putinzena@gmail.com>
2026-07-08 11:46:51 +0700

    Add files via upload

---
+++ CSV_JSON/src/main/java/ru/netology/Main.java
@@ -0,0 +1,56 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        writeString(json, "data.json");
+    }
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        try (FileReader reader = new FileReader(fileName, StandardCharsets.UTF_8)) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+
+            CsvToBean<Employee> csvToBean = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+
+            return csvToBean.parse();
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        return gson.toJson(list, listType);
+    }
+
+    public static void writeString(String json, String fileName) {
+        try (FileWriter writer = new FileWriter(fileName, StandardCharsets.UTF_8)) {
+            writer.write(json);
+            System.out.println("JSON successfully written to file: " + fileName);
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
+}
